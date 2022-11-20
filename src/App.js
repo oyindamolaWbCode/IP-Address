14b2723668af02e15597ed70cee673733084df57
OyindamolaWbCode <kushimo.oyindamola@gmail.com>
2022-11-20 18:45:13 +0100

    install react leaflet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-
 import "./App.css";
 import { FaGreaterThan } from "react-icons/fa";
+import { MapContainer, TileLayer, useMap,Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import icon from "./icon";
+
 //import { useState } from "react";
 
 //https://geo.ipify.org/api/v2/country,city?apiKey=at_tpKQZDz5p0yfpXsrs9SY9INCQf4Yk&ipAddress=8.8.8.8
 
 function App() {
-
   return (
     <div className="App">
       <div className="header">
-         <h4 style={{color: "white", padding: "30px"}}>IP Address Tracker</h4>
-         <div className="in-flex">
-          <input type="text" placeholder="Search for any IP address or domain" />
+        <h4 style={{ color: "white", padding: "30px" }}>IP Address Tracker</h4>
+        <div className="in-flex">
+          <input
+            type="text"
+            placeholder="Search for any IP address or domain"
+            name="ipaddress"
+            id="ipaddress"
+          />
           <div className="search-box">
-            <div className="search-container">
-            < FaGreaterThan />
-            </div>
-            </div>
-         </div>
+            <button className="submit">
+              <FaGreaterThan />
+            </button>
+          </div>
+        </div>
       </div>
       <div className="search-Info">
         <div className="address">
@@ -37,8 +44,18 @@ function App() {
           <h5>ISP</h5>
           <h3>SpaceX / Starlink</h3>
         </div>
-      </div>
-      <div id="map"></div>
+        </div>
+      <MapContainer style={{ height: "600px", width: "100%", position: "absolute", zIndex: "-2"}} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker icon={icon} position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
